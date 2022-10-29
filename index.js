114bf62820474bed92d4000f68c03b39c6cd18ee
thisdotLolu <davideb63@gmail.com>
2022-10-29 20:30:25 +0100

    added styling and sync spinner with timer logic

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const durationInput=document.querySelector('#duration')
 const startButton=document.querySelector('#start')
 const pauseButton=document.querySelector('#pause')
+const circle=document.querySelector('circle')
 
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray',perimeter)
+
+
+// let currentOffset=0;
+let duration
 const timer = new Timer(durationInput, startButton, pauseButton,{
-    onStart(){
-        console.log('timer,started')
+    onStart(totalDuration){
+        // console.log('timer,started')
+        duration=totalDuration;
     },
-    onTick(){
-        console.log('timer just ticked down')
+    onTick(timeRemaining){
+        // console.log('timer just ticked down') 
+        circle.setAttribute('stroke-dashoffset',
+        perimeter * timeRemaining/duration - perimeter
+        )
+        // currentOffset=currentOffset-1
+
     },
     onComplete(){
         console.log('Timer is completed')
